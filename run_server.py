44d694f181a64633e9a4f3d6080050f9b3b5ea64
David <david.smitmanis@gmail.com>
2015-02-17 21:00:48 +0100

    Adds logging, refactors json parsing

--- run_server.py
+++ run_server.py
@@ -3,13 +3,24 @@
 import tornado.ioloop
 import tornado.web
 import tornado.process
+import tornado.options
 import json
 
-
-class OmxHandler(tornado.web.RequestHandler):
+class JsonHandler(tornado.web.RequestHandler):
     def prepare(self):
-        self.data = json.loads(self.request.body)
+        if self.request.method == "POST":
+            try:
+                self.data = json.loads(self.request.body)
+            except ValueError:
+                self.set_status(400)
+                self.write({"status": "Failure", "error": "Malformed JSON"})
+                self.finish()
+                
 
+class SystemHandler(tornado.web.RequestHandler):
+    pass
+
+class OmxHandler(JsonHandler):
     def post(self):
         stream_uri = "%s://%s:%s" % (self.data["protocol"], self.data["uri"], self.data["port"])
         omx_command = ["omxplayer", "-o", "hdmi", "--timeout",
@@ -18,6 +29,8 @@ class OmxHandler(tornado.web.RequestHandler):
         self.write({"status": "OK"})
 
 if __name__ == "__main__":
-    app = tornado.web.Application([tornado.web.url(r"/play", OmxHandler)])
+    tornado.options.parse_command_line()
+    app = tornado.web.Application([tornado.web.url(r"/play", OmxHandler),
+                                   tornado.web.url(r"/system", SystemHandler)])
     app.listen(8080)
     tornado.ioloop.IOLoop.current().start()
