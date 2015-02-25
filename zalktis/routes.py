ae88f7c49419dfe7e0694eb8d5f78eb16f6a23eb
David <david.smitmanis@gmail.com>
2015-02-25 21:04:23 +0100

    Change player route name

--- zalktis/routes.py
+++ zalktis/routes.py
@@ -2,7 +2,7 @@ import tornado.web
 import zalktis.handlers
 
 routes = [
-    tornado.web.url(r"/play", zalktis.handlers.OmxHandler),
+    tornado.web.url(r"/player", zalktis.handlers.OmxHandler),
     tornado.web.url(r"/system", zalktis.handlers.SystemHandler),
     tornado.web.url(r"/", zalktis.handlers.IndexHandler)
 ]
