3e415e5d884be904fc7d1a42297819dc3a317ca1
David <david.smitmanis@gmail.com>
2015-05-15 22:30:04 +0200

    Reactify the SVTPlay stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,54 +1,92 @@
+var zalktis = new Zalktis();
+
+var VideoSelector = React.createClass({
+    displayName: "VideoSelector",
+
+    getInitialState: function () {
+        return {
+            shows: [],
+            videos: []
+        };
+    },
+
+    componentDidMount: function () {
+        zalktis.svtplay.get_all_shows().then(function (shows) {
+            this.setState({
+                shows: shows
+            });
+        }.bind(this));
+    },
+
+    _onChange: function (e) {
+        zalktis.svtplay.get_episodes_for_show({
+            show_url: this.state.shows[e.target.value].url
+        }).then(function (episodes) {
+            this.setState({
+                videos: episodes
+            });
+        }.bind(this));
+    },
+    
+    render: function () {
+        var children = [
+            React.DOM.select({
+                onChange: this._onChange,
+                className: "action-select"
+            }, this.state.shows.map(function (show, i) {
+                return React.DOM.option({value: i}, show.title)
+            }))
+        ].concat(this.state.videos.map(function (video) {
+            return React.createElement(VideoItem, {
+                data: video
+            })
+        }));
+        return (
+            React.DOM.div(
+                null,               
+                children              
+            )  
+        );
+    }
+});
+
+
+var VideoItem = React.createClass({
+    displayName: "VideoItem",
+    _onClick: function (e) {
+        zalktis.svtplay.get_video_url_for_episode({
+            episode_url: this.props.data.url
+        }).then(function (url) {
+            zalktis.player.play({
+                "protocol": "http",
+                "uri": url.replace("http://", "")
+            });
+        }.bind(this));
+    },
+    render: function () {
+        return (
+            React.DOM.a({
+                onClick: this._onClick,
+                className: "video-thumb"
+            }, React.DOM.img({
+                src: this.props.data.thumbnail
+            }), React.DOM.span(
+                null,
+                this.props.data.title
+            ))
+        );
+    }
+});
+
 function main() {
-    var zalktis = new Zalktis();
     document.querySelector("#shutdown-button").addEventListener("click", function () {
 	zalktis.system.shutdown();
     });
     document.querySelector("#svtplay-button").addEventListener("click", function () {
 	var actionArea = document.querySelector("#action-area");
         actionArea.style.left = "0";
-        var showSelect = document.createElement("select");
-        showSelect.className = "action-select";
-        actionArea.appendChild(showSelect);
-        zalktis.svtplay.get_all_shows().then(function (shows) {
-            for (var i=0; i < shows.length; i++) {
-                var o = document.createElement("option");
-                o.value = i;
-                o.innerHTML = shows[i].title;
-                showSelect.appendChild(o);
-            }
-            showSelect.addEventListener("change", function (e) {
-                zalktis.svtplay.get_episodes_for_show({
-                    show_url: shows[e.target.value].url
-                }).then(function (episodes) {
-                    for (var i=0; i < episodes.length; i++) {
-                        var a = document.createElement("a");
-                        a.className = "video-thumb";
-                        a.dataset.url = episodes[i].url;
-
-                        var thumb = document.createElement("img");
-                        thumb.src = episodes[i].thumbnail;
-                        a.appendChild(thumb);
-
-                        var text = document.createElement("span");
-                        text.innerHTML = episodes[i].title;
-                        a.appendChild(text);
-                        
-                        actionArea.appendChild(a);
-                        a.addEventListener("click", function (e) {
-
-                            zalktis.svtplay.get_video_url_for_episode({
-                                episode_url: e.currentTarget.dataset.url
-                            }).then(function (url) {
-                                zalktis.player.play({
-                                    "protocol": "http",
-                                    "uri": url.replace("http://", "")
-                                });
-                            });
-                        });
-                    }
-                });
-            });
-        });
+        React.render(React.createElement(VideoSelector, null),
+                     actionArea);
     });
 }
 
