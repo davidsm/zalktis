ab3848ded3f97c97d2c5698d533ebf8e3290e1d5
David <david.smitmanis@gmail.com>
2015-06-23 23:38:09 +0200

    Add JS module for remote control functionality

--- static/js/control.js
+++ static/js/control.js
@@ -1,93 +1,57 @@
-var zalktis = new Zalktis();
+var RemoteControl;
 
-var VideoSelector = React.createClass({
-    displayName: "VideoSelector",
+(function () {
+    "use strict";
 
-    getInitialState: function () {
-        return {
-            shows: [],
-            videos: []
-        };
-    },
+    var wsServer = "ws://" + window.location.host + "/controller";
 
-    componentDidMount: function () {
-        zalktis.svtplay.get_all_shows().then(function (shows) {
-            this.setState({
-                shows: shows
-            });
-        }.bind(this));
-    },
-
-    _onChange: function (e) {
-        zalktis.svtplay.get_episodes_for_show({
-            show_url: this.state.shows[e.target.value].url
-        }).then(function (episodes) {
-            this.setState({
-                videos: episodes
-            });
-        }.bind(this));
-    },
-    
-    render: function () {
-        var children = [
-            React.DOM.select({
-                onChange: this._onChange,
-                className: "action-select"
-            }, this.state.shows.map(function (show, i) {
-                return React.DOM.option({value: i}, show.title)
-            }))
-        ].concat(this.state.videos.map(function (video) {
-            return React.createElement(VideoItem, {
-                data: video
-            })
+    function sendMessage(event, args, ws) {
+        ws.send(JSON.stringify({
+            event: event,
+            args: args
         }));
-        return (
-            React.DOM.div(
-                null,               
-                children              
-            )  
-        );
     }
-});
 
+    function createConnection(ws) {
+        var listeners = {};
 
-var VideoItem = React.createClass({
-    displayName: "VideoItem",
-    _onClick: function (e) {
-        zalktis.svtplay.get_video_url_for_episode({
-            episode_url: this.props.data.url
-        }).then(function (url) {
-            zalktis.player.play({
-                "protocol": "http",
-                "uri": url.replace("http://", "")
+        var connection = {
+            on: function (event, callback) {
+                listeners[event] = listeners[event] || [];
+                listeners[event].push(callback);
+            },
+
+            emit: function (event, args) {
+                sendMessage(event, args, ws);
+            }
+        };
+
+        ws.onmessage = function (message) {
+            var parsedMessage = JSON.parse(message.data);
+            var event = parsedMessage.event;
+            var args = parsedMessage.args;
+            var registeredListeners = listeners[event];
+            if (registeredListeners) {
+                for (var i=0; i < registeredListeners.length; i++) {
+                    registeredListeners[i](args);
+                }
+            }
+        };
+
+        return connection;
+    };
+
+    RemoteControl = {
+        connect: function (type) {
+            var ws = new WebSocket(wsServer);
+            return new CrapPromise(function (resolve, reject) {
+                ws.onopen = function () {
+                    sendMessage("join", {
+                        client_type: type
+                    }, ws);
+                };
+                resolve(createConnection(ws));
             });
-        }.bind(this));
-    },
-    render: function () {
-        return (
-            React.DOM.a({
-                onClick: this._onClick,
-                className: "video-thumb"
-            }, React.DOM.img({
-                src: this.props.data.thumbnail
-            }), React.DOM.span(
-                null,
-                this.props.data.title
-            ))
-        );
-    }
-});
-
-function main() {
-    document.querySelector("#shutdown-button").addEventListener("click", function () {
-	zalktis.system.shutdown();
-    });
-    document.querySelector("#svtplay-button").addEventListener("click", function () {
-	var actionArea = document.querySelector("#action-area");
-        actionArea.style.left = "0";
-        React.render(React.createElement(VideoSelector, null),
-                     actionArea);
-    });
-}
-
-document.addEventListener("DOMContentLoaded", main);
+        }
+    };
+})();
