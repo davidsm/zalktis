55e1153d1f44e8265e8dbbc895b2445fb392a5a5
David <david.smitmanis@gmail.com>
2015-07-30 21:44:52 +0200

    Clean up results from SVTScraper a tad

--- zalktis/lib/svtscraper.py
+++ zalktis/lib/svtscraper.py
@@ -10,20 +10,27 @@ _URL_ALL_SHOWS = "http://www.svtplay.se/ajax/sok/forslag.json"
 
 
 class SVTScraper(object):
-    
+
+    def _format_show(self, show):
+        show.pop("isGenre", None)
+
+        # Convert all to absolute URLs
+        # Some in the listing are absolute, some are relative
+        if not show["url"].startswith("http"):
+            show["url"] = "%s%s" % (_URL_BASE, show["url"])
+        return show
+
     @tornado.gen.coroutine
     def get_all_shows(self):
         response = yield AsyncHTTPClient().fetch(_URL_ALL_SHOWS)
         parsed_response = json.loads(response.body)
-        raise tornado.gen.Return(parsed_response)
+        all_shows = [self._format_show(show) for show in parsed_response
+                     if show["isGenre"] != "genre"]
+        # Sort by title here rather than on client (should be cheaper)
+        raise tornado.gen.Return(sorted(all_shows, key=lambda s: s["title"]))
 
     @tornado.gen.coroutine
     def get_episodes_for_show(self, show_url):
-        # Some URLs from the listing are absolute, some relative
-        # Deal with that here rather than earlier for now
-        # Also, should use urlparse when cleaning this up
-        if not show_url.startswith("http"):
-            show_url = "%s%s" % (_URL_BASE, show_url)
         response = yield AsyncHTTPClient().fetch(show_url)
         episode_links = BeautifulSoup(response.body).find(id="more-episodes-panel").find_all("a", href=re.compile("/video/"))
         episodes = [{"title": a.find("span", class_="play_videolist-element__title-text").get_text().strip(), "url": a["href"], "thumbnail": a.find("img")["src"]} for a in episode_links]
@@ -37,4 +44,3 @@ class SVTScraper(object):
         for video_listing in parsed_response["video"]["videoReferences"]:
             if video_listing["playerType"] == "ios":
                 raise tornado.gen.Return(video_listing["url"])
-
