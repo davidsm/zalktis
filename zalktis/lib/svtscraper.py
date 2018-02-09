7060a390c542a9aa5b7a3236f8ff4c14a6651ff1
David <david.smitmanis@gmail.com>
2018-02-09 23:04:13 +0100

    Handle missing thumbnail

--- zalktis/lib/svtscraper.py
+++ zalktis/lib/svtscraper.py
@@ -41,6 +41,8 @@ class SVTScraper(object):
         return "%s/%s" % (_URL_VIDEO_API_BASE, relative_url)
 
     def _build_thumbnail_url(self, response_url):
+        if not response_url:
+            return ""
         if response_url.startswith("//"):
             prefix = "https:"
         else:
